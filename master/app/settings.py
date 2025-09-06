79122262dbe91d14a6aecf21bb3ad1fd90f8bc50
jjvasquezg <juanjos137k@gmail.com>
2025-09-06 17:47:02 -0500

    Modificacion de settings.py en ambos

--- master/app/settings.py
+++ master/app/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     REST_HOST: str = "0.0.0.0"
--- worker/app/settings.py
+++ worker/app/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     GRPC_HOST: str = "0.0.0.0"
